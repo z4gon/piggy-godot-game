e4f6624262dfc7c7dde4a7c21299b327be5cc0fc
z4gon <4588601+z4gon@users.noreply.github.com>
2023-02-08 22:09:41 -0300

    pig movement script

---
+++ Scripts/Pig.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var PIG_SPEED = 100
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var speed = PIG_SPEED * delta
+	
+	if Input.is_action_pressed("ui_right"):
+		move(speed, 0)
+	if Input.is_action_pressed("ui_left"):
+		move(-speed, 0)
+	if Input.is_action_pressed("ui_up"):
+		move(0, -speed)
+	if Input.is_action_pressed("ui_down"):
+		move(0, speed)
+
+func is_action(name: String):
+	pass
+	
+func move(dx: float, dy: float):
+	position.x += dx
+	position.y += dy
